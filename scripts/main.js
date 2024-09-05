f57dd91ffbbd0c1e52ab3f331d7181fed417de07
MPBDev <132145767+MPBCoder@users.noreply.github.com>
2024-09-05 16:14:05 +0330

    format header and finish header

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,13 @@
-const nav = document.querySelector(".nav")
-const headerBtn = document.querySelector(".header__menu-btn")
-let isNavOpened = false
+const nav = document.querySelector(".nav");
+const headerBtn = document.querySelector(".header__menu-btn");
+let isNavOpened = false;
 
-document.addEventListener("click", function() {
+document.addEventListener("click", function () {
     if (isNavOpened) {
-        nav.classList.remove("nav--open")
-        isNavOpened = false
+        nav.classList.remove("nav--open");
+        isNavOpened = false;
+    } else {
+        nav.classList.add("nav--open");
+        isNavOpened = true;
     }
-    else {
-        nav.classList.add("nav--open")
-        isNavOpened = true
-    }
-})
+});
