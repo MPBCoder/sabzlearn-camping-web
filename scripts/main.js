fd6d1b2343a2ef4c40f0f4d74438525c4086ad16
MPBDev <132145767+MPBCoder@users.noreply.github.com>
2024-09-06 18:06:35 +0330

    create destinations

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@ const nav = document.querySelector(".nav");
 const headerBtn = document.querySelector(".header__menu-btn");
 let isNavOpened = false;
 
-document.addEventListener("click", function () {
+headerBtn.addEventListener("click", function () {
     if (isNavOpened) {
         nav.classList.remove("nav--open");
         isNavOpened = false;
